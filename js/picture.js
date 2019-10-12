bc4183c605a54552158ab7cd6ed51aa72d14c547
Sergei Pavlov <unplanetx@gmail.com>
2019-10-12 17:58:01 +1000

    Выполняет задание 11

---
+++ js/picture.js
@@ -0,0 +1,18 @@
+'use strict';
+
+(function () {
+
+  var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+  window.picture = {
+    renderPicture: function (picture) {
+      var pictureElement = pictureTemplate.cloneNode(true);
+      pictureElement.querySelector('.picture__img').src = picture.url;
+      pictureElement.querySelector('.picture__likes').textContent = picture.likes;
+      pictureElement.querySelector('.picture__comments').textContent = picture.comments.length;
+
+      return pictureElement;
+    }
+  };
+
+})();
